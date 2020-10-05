1c4f90fa43473ad3ed67297cd49f8336515677c9
aefuen1 <aefuen1@gmail.com>
2020-10-05 08:57:58 -0400

    Updates

---
+++ precache-manifest.8bd86387960229dee5d0c5c8b8af6729.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42765580429d107e73e5e329c5b5d5da",
+    "url": "/calculadora/index.html"
+  },
+  {
+    "revision": "1b720a9a0e7323fcfd26",
+    "url": "/calculadora/static/css/main.ac074969.chunk.css"
+  },
+  {
+    "revision": "ff192bcb16e142ff21e9",
+    "url": "/calculadora/static/js/2.235ef652.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculadora/static/js/2.235ef652.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b720a9a0e7323fcfd26",
+    "url": "/calculadora/static/js/main.29faa950.chunk.js"
+  },
+  {
+    "revision": "10a74285f2769b94f6ac",
+    "url": "/calculadora/static/js/runtime-main.91648a95.js"
+  }
+]);
